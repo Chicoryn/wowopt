ff85fc161a3377ec26c386212db13343073d45bd
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2011-11-18 19:58:01 +0100

    Spirit -> MP5 for T12 script

--- constraints/t12_set_bonus.py
+++ constraints/t12_set_bonus.py
@@ -3,15 +3,15 @@ t12_items = [ item for item in self.get_items() if item.item_id in t12_items ]
 
 if len(t12_items) >= 2:
     t12_2p_used = pulp.LpVariable(N('t12_2p_used'), 0, 1, 'Integer')
-    t12_2p_spirit = pulp.LpVariable(N('t12_2p_spirit'), 0)
+    t12_2p_mp5 = pulp.LpVariable(N('t12_2p_mp5'), 0)
     t12_4p_used = pulp.LpVariable(N('t12_4p_used'), 0, 1, 'Integer')
     t12_4p_hps = pulp.LpVariable(N('t12_4p_hps'), 0)
 
     problem += pulp.lpSum([ 0.25 * self.used[item] for item in t12_items ]) >= t12_4p_used
     problem += pulp.lpSum([ 0.5 * self.used[item] for item in t12_items ]) >= t12_2p_used
 
-    problem += t12_2p_spirit == self.total_stats[I['spirit']] + 411 * t12_2p_used
+    problem += t12_2p_mp5 == self.total_stats[I['mp5']] + 411 * t12_2p_used
     problem += t12_4p_hps == self.total_stats[I['hps']] + 400 * t12_4p_used
 
-    self.total_stats[I['spirit']] = t12_2p_spirit
+    self.total_stats[I['mp5']] = t12_2p_mp5
     self.total_stats[I['hps']] = t12_4p_hps
